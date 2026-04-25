9bf4c6633b94443d4636cf60e9690007ae42d126
ftzenra <ftzenra@gmail.com>
2026-04-25 21:43:26 +0800

    umm reward to

--- game porject/randomizer.py
+++ game porject/randomizer.py
@@ -1,4 +1,4 @@
-
+import utility
 import random
 import Datamodule
 
@@ -17,3 +17,27 @@ w1 = scrambled_words[0]
 w2 = scrambled_words[1]
 w3 = scrambled_words[2]
 w4 = scrambled_words[3]
+
+def gifts():
+    rewards = [utility.health, utility.hints, utility.word_pass]
+    pick_reward = random.choice(rewards)
+    
+    if pick_reward == utility.health:
+        utility.health += 1
+        reward_type = "health"
+    elif pick_reward == utility.hints:
+        utility.hints += 1
+        reward_type = "hints"
+    elif pick_reward == utility.word_pass:
+        utility.word_pass += 1
+        reward_type = "word_pass"
+    
+    return reward_type, pick_reward
+reward_type, reward_value = gifts()
+
+
+
+
+
+
+
--- game porject/utility.py
+++ game porject/utility.py
@@ -10,34 +10,34 @@ revealed_count = 0
 current_index = 0
 correct_words = [False, False, False, False]
 
+# pang reset so everytime mag laro di same value
 def reset_game():
-    """Reset all game variables for a new game"""
     health = 3
     hints = 1
     word_pass = 0
     score = 0
     revealed_count = 0
+    # pang reload to ng randomizer para di same value pag nag loop ung laro
     importlib.reload(randomizer)
-    
+# eto func ko pang clear screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
-
+#rulesss
 def rules():
     print("Unscramble the letters to form a word.")
     print("You have 3 health points and gain rewards for correct answers.")
     print("rewards can be,health,hints,or points, level pass")
-
+#welcome dialogues
 def welcome():
     print("Welcome to the Word Scramble Game!")
     print("Try to unscramble the words and earn points.")
-
+#pang show ng qoutes
 def show_quotes():
     print(f"here quote you created{ randomizer.quote()}")
-   
-
+# pang check if na return na 4 word value
 def cheacker():
     return all(correct_words)
-
+# eto current na na letter sa scrambled, gamit to sa hint para mabigay mga letters
 def current():
     return randomizer.scrambled_words[current_index]
     
