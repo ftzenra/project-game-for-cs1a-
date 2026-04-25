276130dfd4ad3347884776adb1f11f7e26fdb4a9
Ximeri001 <krisha.nicole.caole@gmail.com>
2026-04-25 21:46:30 +0800

    edited mode 1 & mode 2

--- game porject/Datamodule.py
+++ game porject/Datamodule.py
@@ -1,6 +1,76 @@
 import random
 
-level3 = [
+#_Mode_1
+level1 = ["bike", "love", "face", "dull", "file"]
+
+level2 = ["juice", "onion", "magic", "eagle", "empty"]
+
+level3 = ["square", "guitar", "beauty", "emerge", "adjust"]
+
+level4 = ["absence", "courage", "approve", "diamond", "general"]
+
+level5 = ["boundary", "heritage", "decorate", "marriage", "explicit"]
+
+level6 = ["judgement", "authority", "happiness", "intention", "education"]
+
+level7 = ["prosperity", "dedication", "experience", "appearance", "everything"]
+
+#_Mode_2
+mode2 = [
+    {
+        "words": ["high", "tides", "good", "vibes"],
+        "quote": "High tides, good vibes"
+    },
+    {
+        "words": ["life", "happens", "coffee", "works"], 
+        "quotes": "Life happens, Coffee helps"
+    }, 
+    {
+        "words": ["stop", "wishing", "start", "doing"],
+        "quotes": "Stop Wishing, Start Doing"
+    },
+    {
+        "words": ["push", "harder", "than", "yesterday"], 
+        "quotes": "Push harder than yesterday"
+    }, 
+    {
+        "words": ["courage", "doesnt", "always", "roar"], 
+        "quote": "Courage doesn't always roar"
+    }, 
+    {
+        "words": ["work", "hard", "dream", "big"],
+        "quote": "Work hard, Dream big"
+    },
+    {
+        "words": ["happiness", "depends", "on", "ourselves"],
+        "quotes": "Happiness Depends on Ourselves"
+    },
+    {
+        "words": ["grow", "through", "what", "goes"],
+        "quotes": "Grow throw what goes"
+    },
+    {
+        "words": ["whatever", "happens", "take", "responsibility"],
+        "quotes": "Whatever Happens, Take Responsibility"
+    },
+    {
+        "words": ["face", "challenge", "be", "victorious"],
+        "quote": "Face Challenge, Be Victorious"
+    },
+    {
+        "words": ["look", "before", "you", "leap"],
+        "quote": "Look before you leap"
+    }
+]
+
+m2rewards = [
+    "hp+1", "hint+1", #common
+    "bonus points", "hp+2", "hint+2", "hint+3", #uncommon
+    "hp+3", "pass" #rare
+]
+
+#_Mode3_
+mode3 = [
     {
         "words": ["nothing", "becomes", "possible", "therefore"],
         "quote": "Nothing becomes possible therefore"
@@ -24,5 +94,5 @@ level3 = [
     {
         "words": ["always", "remember", "tomorrow", "victory"],
         "quote": "Always remember tomorrow victory"
-    },]
-
+    },
+]
