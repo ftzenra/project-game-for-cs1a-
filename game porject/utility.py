98be8bc2d4f1d79a5a8e45dac390e1cea86ef0a7
SunieDoongiDori°-^ <ychayhearts@gmail.com>
2026-04-26 14:45:40 +0800

    may enayos lngz

--- game porject/utility.py
+++ game porject/utility.py
@@ -20,37 +20,31 @@ def reset_game():
     revealed_count = 0
     # pang reload to ng randomizer para di same value pag nag loop ung laro
     importlib.reload(randomizer)
+
 # eto func ko pang clear screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+    
 #rulesss
 def rules():
     print("Game Rules:\n")
     print('''You have 3 health points and 1 Hint!
 Unscramble the letters to form a word and gain rewards for each correct answer!
-Rewards can be health, hints, points, or level pass.''')
+Rewards can be health, hints, points, or even level pass.''')
 
+#welcome dialogues
 def welcome():
     print("Welcome to Word Scramble Game!")
     print("Unscramble the words to earn points and rewards!")
 
+#pang show ng quotes
 def show_quotes():
     print(f"here is the quote you created: { randomizer.quote()}")
-   
 
-    print("Unscramble the letters to form a word.")
-    print("You have 3 health points and gain rewards for correct answers.")
-    print("rewards can be,health,hints,or points, level pass")
-#welcome dialogues
-def welcome():
-    print("Welcome to the Word Scramble Game!")
-    print("Try to unscramble the words and earn points.")
-#pang show ng qoutes
-def show_quotes():
-    print(f"here quote you created{ randomizer.quote()}")
 # pang check if na return na 4 word value
 def cheacker():
     return all(correct_words)
+
 # eto current na na letter sa scrambled, gamit to sa hint para mabigay mga letters
 def current():
     return randomizer.scrambled_words[current_index]
